5bc621e76b02e97725ce68f6a95e08d9d92ea3bf
Jeffrey <jvbijleveld@gmail.com>
2017-12-28 22:13:14 +0100

    refined logging, added env files

--- app.js
+++ app.js
@@ -1,22 +1,24 @@
+require('dotenv').config();
+
 const log4js = require('log4js');
 const schedule = require('node-schedule');
 const request = require('request');
 const strava = require('./lib/strava');
  
 log4js.configure({appenders: {
-	filelog: { type: 'file', filename: './log/app.log' },
-	out: { type: 'stdout' }
-},
-categories: {
-	default: { appenders: [ 'out' ], level: 'debug' }
-}
+		filelog: { type: 'file', filename: './log/app.log' },
+		out: { type: 'stdout' },
+		'errors': {type: 'logLevelFilter', appender: 'filelog', level: process.env.LOG_LEVEL}
+	},
+	categories: {
+		default: { appenders: [ 'out', 'errors' ], level: 'debug' }
+	}
 });
 
 logger = log4js.getLogger();
 
-var j = schedule.scheduleJob('*/10 * * * * *', function(){
-  logger.debug('Running scheduler');
+var j = schedule.scheduleJob(process.env.SCHEDULER_STRING, function(){
+  logger.debug('Running scheduled task');
   
   strava.getActivities();
-  
 });
