75237912febe8febd78f2f4bb71842026c30584b
jeffrey <jeffrey@vanbijleveld.nl>
2018-01-03 06:34:38 -0500

    added server: listing activities from mongo

--- app.js
+++ app.js
@@ -2,11 +2,17 @@ require('dotenv').config();
 
 const log4js = require('log4js');
 const schedule = require('node-schedule');
-const request = require('request');
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
 const strava = require('./lib/strava');
- 
+
+var app = express();
+const hostname = '127.0.0.1';
+const port = 3000;
+
 log4js.configure({appenders: {
-		filelog: { type: 'file', filename: './log/app.log' },
+		filelog: { type: 'file', filename: './log/app.log', maxLogSize: 10485760, backups: 3, compress: true },
 		out: { type: 'stdout' },
 		'errors': {type: 'logLevelFilter', appender: 'filelog', level: process.env.LOG_LEVEL}
 	},
@@ -17,8 +23,28 @@ log4js.configure({appenders: {
 
 logger = log4js.getLogger();
 
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname,'views'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.get('/', function(req, resp){
+	strava.fetchCurrentYear(function(activities){
+		resp.render('index', {
+			title: 'Hello world',
+			activities: activities
+		});	
+	});
+});
+
+app.listen(port, function(){
+	logger.info('Server started on port ' + port);
+});
+
 var j = schedule.scheduleJob(process.env.SCHEDULER_STRING, function(){
   logger.debug('Running scheduled task');
   
-  strava.syncAllActivities();
+  strava.syncAllActivities(function(){
+		logger.info('Activity Sync done');
+	});
 });
